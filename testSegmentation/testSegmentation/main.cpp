3ddefe8393acdd48a998fdf6d5bfaf9925a11259
Mridul Gupta <mridulg@users.noreply.github.com>
2016-05-02 05:18:07 +0530

    create main.cpp

---
+++ testSegmentation/testSegmentation/main.cpp
@@ -0,0 +1,16 @@
+//
+//  temp.cpp
+//  testSegmentation
+//
+//  Created by Mridul Gupta on 4/1/16.
+//  Copyright © 2016 Mridul Gupta. All rights reserved.
+//
+
+//
+//  References : http://paulbourke.net/geometry/circlesphere/
+//               http://s-ln.in/2013/04/18/hand-tracking-and-gesture-detection-opencv/
+//               http://docs.opencv.org/2.4/doc/tutorials/imgproc/shapedescriptors/find_contours/find_contours.html
+//               http://docs.opencv.org/2.4/doc/tutorials/imgproc/shapedescriptors/hull/hull.html
+//
+//
+

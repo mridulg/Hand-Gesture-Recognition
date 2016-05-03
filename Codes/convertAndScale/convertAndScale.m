0bdc3f28829357aca2eccb253eec54dbe76f6b3e
Mridul Gupta <mridul15061@iiitd.ac.in>
2016-05-03 05:59:48 +0530

    remove all comments

--- Codes/convertAndScale/convertAndScale.m
+++ Codes/convertAndScale/convertAndScale.m
@@ -1,15 +1,10 @@
 for i=0:9
 	for j=1:55
 		path='/Users/mridul/HGR/Datasets/myDataset/';
-		path;
 		x=strcat(path,num2str(i),'_',num2str(j),'.png');
-		%x;
 		img=imread(x);
-		%imshow(image);
 		scaled=imresize(image,[64 64]);
-		%imshow(scaled)
 		destination=strcat('/Users/mridul/HGR/Datasets/scaledDataset/',num2str(i),'_', num2str(j), '.png');
-		%destination
 		imwrite(scaled,destination);
 	end
 end
@@ -17,15 +12,10 @@ end
 for i='a':'z'
 	for j=1:55
 		path='/Users/mridul/HGR/Datasets/myDataset/';
-		path;
 		x=strcat(path,i),'_',num2str(j),'.png');
-		%x;
 		img=imread(x);
-		%imshow(image);
 		scaled=imresize(image,[64 64]);
-		%imshow(scaled)
 		destination=strcat('/Users/mridul/HGR/Datasets/scaledDataset/',i,'_', num2str(j), '.png');
-		%destination
 		imwrite(scaled,destination);
 	end
 end
